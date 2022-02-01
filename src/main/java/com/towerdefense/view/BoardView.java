79c6c64653605732ac7f08fa48c2fe96bcf84a5b
Mojigot7 <ayyacine.pro@gmail.com>
2022-02-01 23:10:42 +0100

    début de plateau

--- src/main/java/com/towerdefense/view/BoardView.java
+++ src/main/java/com/towerdefense/view/BoardView.java
@@ -2,12 +2,31 @@ package com.towerdefense.view;
 
 import javax.swing.JPanel;
 
+import com.towerdefense.model.Units;
+import java.awt.Color;
+import java.awt.Graphics;
+
+
 public class BoardView extends JPanel {
 
-    private UnitsView[][] cases;
+    private Units[][] cases;
 
     public BoardView() {
         
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        for(int x = 0; x < 20; x++) {
+        	for(int y = 0; y < 20; y++) {
+        		g.setColor(Color.BLACK);
+                g.drawRect(y*32, x*32, 32, 32);
+        	}
+        }
+        
+        repaint();
+    }
+
     
 }
