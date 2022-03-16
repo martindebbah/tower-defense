c9d519c02b4677ef0166e3f4c852cf5b08e9f4dc
DEBBAH martin <debbahmartin@gmail.com>
2022-03-16 20:11:49 +0100

    Correction d'erreurs

--- src/main/java/com/towerdefense/view/Window.java
+++ src/main/java/com/towerdefense/view/Window.java
@@ -1,22 +1,9 @@
 package com.towerdefense.view;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import com.towerdefense.model.Game;
-import com.towerdefense.model.Player;
-import com.towerdefense.model.Wave;
 import com.towerdefense.view.menu.*;
 
-import java.awt.BorderLayout;
-
 public class Window extends JFrame {
-
-    private BoardView board;
-    private Shop shop;
-    private Game game;
-    private Wave wave;
 
     public Window() {
 
@@ -27,21 +14,22 @@ public class Window extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
 
-        //setAccueil();
+        setAccueil();
 
         // test
-        play();
+        //play();
 
     }
 
     public void setAccueil() {
-        setContentPane(new Accueil(this));
+        getContentPane().add(new Accueil(this));
         revalidate();
         repaint();
     }
 
     public void setMenu() {
-        setContentPane(new Menu(this));
+        getContentPane().removeAll();
+        getContentPane().add(new Menu(this));
         revalidate();
         repaint();
     }
@@ -58,53 +46,15 @@ public class Window extends JFrame {
         repaint();
     }
 
-    private void createBoard() { // Créer une vue pour le plateau
-        board = new BoardView(game, shop);
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 30));
-        panel.add(board);
-        add(panel, BorderLayout.WEST);
-    }
-
-    private void createShop() {
-        shop = new Shop();
-        add(shop, BorderLayout.EAST);
+    public void refresh() {
+        revalidate();
+        repaint();
     }
 
     public void play() {
-        // Création du plateau de jeu
-        Player player = new Player("Martin");
-        this.game = new Game(32, 20, player);
-        setLayout(new BorderLayout());
-        this.wave = new Wave(this.game,60);
-        WaveView w = new WaveView(wave);
-        createShop();
-        createBoard();
-        add(w, BorderLayout.NORTH);
-        revalidate();
-        repaint();
-
-        while(player.isAlive() && wave.getCurrentWave() <= wave.getNbWaves()){
-            if(wave.getFinChrono()){
-                wave.incrementWave();
-                System.out.println(wave.getCurrentWave());
-                wave.setFinChrono(false);
-            }
-            playRound();
-        }
-
-        wave.setFinishGame(true);
-    }
-
-    public void playRound() {
-        try {
-            Thread.sleep(50);
-        }catch(InterruptedException ie) {
-            ie.printStackTrace();
-        }
-        wave.play();
-        revalidate();
-        repaint();
+        GameView gameView = new GameView(this);
+        setContentPane(gameView);
+        gameView.start();
     }
     
 }
