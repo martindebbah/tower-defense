c16b2b4ca485ceb7f13352bd2dfb38524580511e
Feriel Bouzaienne <ferilbouzaienne@gmail.com>
2022-04-13 06:35:43 +0200

    Discover

--- src/main/java/com/towerdefense/view/menu/Discover.java
+++ src/main/java/com/towerdefense/view/menu/Discover.java
@@ -13,26 +13,26 @@ public class Discover {
 	private ImageIcon img ;
 	private JLabel labelDisc ; 
 	private JFrame disco ;
-	private JButton back ;
+	private JButton next ;
 	
 	public Discover(){
 		
-		 img= new ImageIcon("src/main/resources/images/accueil2.jpeg");
+		 img= new ImageIcon("src/main/resources/images/disc1.png");
 		   labelDisc=new JLabel(img);
-		   labelDisc.setSize(650,450);
+		   labelDisc.setSize(700,530);
 		   
-		   back= new JButton("Back");
-		    back.setBackground(Color.BLUE); 
-			back.setBounds(60,300,80,40);
-			labelDisc.add(back);
-			back.addActionListener(new ActionListener() {
+		   next= new JButton("Back");
+		    next.setBackground(Color.BLUE); 
+			next.setBounds(60,300,80,40);
+			labelDisc.add(next);
+			next.addActionListener(new ActionListener() {
 				   public void actionPerformed(ActionEvent e) {
 					 //new MenuEssai();
 				   }	   
 			}
 					 );
 		   disco= new JFrame("You need help to discover the game?Here we go!");
-		   disco.setSize(650,450);
+		   disco.setSize(1000,1000);
 		   disco.add(labelDisc);
 		   disco.setLayout(null);
 		   disco.setLocationRelativeTo(null);
