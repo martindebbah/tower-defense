fb7aabde11b970eab7b958d83bf508f11414feb2
Suishy77 <dorian.bortolucci@gmail.com>
2022-04-28 20:50:48 +0200

    pull

--- src/main/java/com/towerdefense/view/menu/Menu.java
+++ src/main/java/com/towerdefense/view/menu/Menu.java
@@ -7,18 +7,20 @@ import java.awt.GridLayout;
 
 import com.towerdefense.view.Window;
 
+import javafx.scene.text.Font;
+
 public class Menu extends JPanel {
-	
+
 	private JPanel buttons;
-    
-    public Menu(Window window) {			 
+
+	public Menu(Window window) {
 		setSize(1000, 1000);
-		
+
 		buttons = new JPanel();
 		buttons.setSize(1000, 1000);
 		add(buttons);
 		buttons.setLayout(new GridLayout(0, 1));
-		
+
 		JButton newGame = new JButton("Nouvelle partie");
 		newGame.addActionListener(e -> {
 			window.setNewGame();
